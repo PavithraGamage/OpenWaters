5f0c935752545a4da33fdbfade1151a0f895552e
pavithra-hv <pavithra@helios-ventures.co>
2023-04-22 08:13:32 +0530

    Js nav drop down implemented

---
+++ js/home.js
@@ -0,0 +1,11 @@
+// platform drop down start --------------------------------
+function showBox() {
+  var box = document.getElementById("box");
+  box.style.display = "flex";
+}
+
+function hideBox() {
+  var box = document.getElementById("box");
+  box.style.display = "none";
+}
+// platform drop down end --------------------------------
