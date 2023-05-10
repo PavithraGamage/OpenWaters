5722ceabd0cea5730ef6b583ebb71335b8856d91
pavithra-hv <pavithragamage916@gmail.com>
2023-05-10 18:53:06 +0530

    mobile menu implemented

--- js/main.js
+++ js/main.js
@@ -39,10 +39,10 @@ function mobile_menu_show() {
   var box = document.getElementById("main-drop-down");
   box.style.display = "block";
 
-  var hamburger = document.getElementById('mobile-nav-expan');
+  var hamburger = document.getElementById("mobile-nav-expan");
   hamburger.style.display = "none";
 
-  var close_btn = document.getElementById('mobile-nav-close-btn');
+  var close_btn = document.getElementById("mobile-nav-close-btn");
   close_btn.style.display = "block";
 }
 
@@ -50,34 +50,120 @@ function mobile_menu_hide() {
   var box = document.getElementById("main-drop-down");
   box.style.display = "none";
 
-  var close_btn = document.getElementById('mobile-nav-close-btn');
+  var close_btn = document.getElementById("mobile-nav-close-btn");
   close_btn.style.display = "none";
 
-  var hamburger = document.getElementById('mobile-nav-expan');
+  var hamburger = document.getElementById("mobile-nav-expan");
   hamburger.style.display = "block";
 
-  var platform_mobile_menu = document.getElementById('mobile-platform');
+  var platform_mobile_menu = document.getElementById("mobile-platform");
   platform_mobile_menu.style.display = "none";
 
   var box = document.getElementById("mobile-logo");
   box.style.display = "block";
 
-  var platform_expan = document.getElementById('mobile-platform-expan');
+  var platform_expan = document.getElementById("mobile-platform-expan");
   platform_expan.style.display = "none";
+
+  var box = document.getElementById("mobile-resource-expan");
+  box.style.display = "none";
+
+  var box = document.getElementById("mobile-resource");
+  box.style.display = "none";
+
+  var box = document.getElementById("mobile-about-expan");
+  box.style.display = "none";
+
+  var box = document.getElementById("mobile-about");
+  box.style.display = "none";
 }
 // mobile main drop down end -----------------------------------------
 
-
+// platform mobile menu start -----------------------------------------
 function platform_mobile_menu_show() {
   var box = document.getElementById("mobile-logo");
   box.style.display = "none";
 
-  var platform_mobile_menu = document.getElementById('mobile-platform');
-  platform_mobile_menu.style.display = "block";
+  var platform_mobile_menu = document.getElementById("mobile-platform");
+  platform_mobile_menu.style.display = "flex";
 
   var main_drop_down = document.getElementById("main-drop-down");
   main_drop_down.style.display = "none";
 
-  var platform_expan = document.getElementById('mobile-platform-expan');
+  var platform_expan = document.getElementById("mobile-platform-expan");
   platform_expan.style.display = "block";
+}
+// platform mobile menu end ------------------------------------------
+
+// platform menu back to main menu start -------------------------------------
+function show_mobile_main_menu_platform() {
+  var box = document.getElementById("mobile-platform-expan");
+  box.style.display = "none";
+
+  var logo = document.getElementById("mobile-logo");
+  logo.style.display = "block";
+
+  var platform_mobile_menu = document.getElementById("mobile-platform");
+  platform_mobile_menu.style.display = "none";
+
+  var main_drop = document.getElementById("main-drop-down");
+  main_drop.style.display = "block";
+}
+// platform menu back to main menu end -----------------------------------------
+
+function resource_mobile_menu_show() {
+  var box = document.getElementById("mobile-resource-expan");
+  box.style.display = "block";
+
+  var hamburger = document.getElementById("main-drop-down");
+  hamburger.style.display = "none";
+
+  var box = document.getElementById("mobile-logo");
+  box.style.display = "none";
+
+  var box = document.getElementById("mobile-resource");
+  box.style.display = "flex";
+}
+
+function show_mobile_main_menu_resources(){
+
+  var box = document.getElementById("mobile-resource-expan");
+  box.style.display = "none";
+
+  var logo = document.getElementById("mobile-logo");
+  logo.style.display = "block";
+
+  var main_drop = document.getElementById("main-drop-down");
+  main_drop.style.display = "block";
+
+  var box = document.getElementById("mobile-resource");
+  box.style.display = "none";
+}
+
+function about_mobile_menu_show(){
+  var hamburger = document.getElementById("main-drop-down");
+  hamburger.style.display = "none";
+
+  var box = document.getElementById("mobile-about-expan");
+  box.style.display = "block";
+
+  var box = document.getElementById("mobile-about");
+  box.style.display = "flex";
+
+  var box = document.getElementById("mobile-logo");
+  box.style.display = "none";
+}
+
+function show_mobile_main_menu_about(){
+  var box = document.getElementById("mobile-about-expan");
+  box.style.display = "none";
+
+  var hamburger = document.getElementById("main-drop-down");
+  hamburger.style.display = "block";
+
+  var box = document.getElementById("mobile-logo");
+  box.style.display = "block";
+
+  var box = document.getElementById("mobile-about");
+  box.style.display = "none";
 }
